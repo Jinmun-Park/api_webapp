368e3c66cb52572bf0eacb7b785a3a54e4dac243
Jinmun-Park <pjm9827@gmail.com>
2021-09-24 23:56:16 +0800

    Flask_setup_day1

---
+++ webapp.py
@@ -0,0 +1,17 @@
+'''
+set FLASK_APP=webapp.py
+$env:FLASK_APP = "webapp"
+$env:FLASK_ENV = "development"
+set FLASK_DEBUG=1
+flask run
+'''
+
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+app.run(debug=True, use_debugger=False, use_reloader=False)
+
+@app.route("/")
+def home_page():
+    return render_template('home.html')
+
