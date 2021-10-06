d86491693dd0e49c73e3b1cec8e1079a4386e92c
Jinmun-Park <pjm9827@gmail.com>
2021-10-06 21:09:18 +0800

    Channel Class Complete Day 8_2

---
+++ src/models/covid_eda.py
@@ -0,0 +1,17 @@
+from src.utils.api import run_covid_api
+import pandas as pd
+
+# ====================== DEFINE FUNCTION ====================== #
+def read_pickle(file_name: str) -> pd.DataFrame:
+    return pd.read_pickle('Pickle/' + file_name)
+
+# ====================== DATA.GO.KR API RUNNING ====================== #
+# Korea Government API
+run_covid_api()
+# ==================================================================== #
+
+# ====================== LOAD PICKLES ====================== #
+df_age_sex = read_pickle('covid_age_sex.pkl')
+df_vaccines = read_pickle('covid_vaccines.pkl')
+df_city = read_pickle('covid_city.pkl')
+df_cases = read_pickle('covid_cases.pkl')
