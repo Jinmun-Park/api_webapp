90dd11732320ce80fd4d7aeeab729e194a0d6a05
Jinmun-Park <pjm9827@gmail.com>
2021-10-20 16:45:09 +0800

    Revert "Kobert Run Completed (Day 10)"
    This reverts commit 0658392893690e073194cdf3803ef54841fd84af.

--- src/models/youtuve_channel.py
+++ src/models/youtuve_channel.py
@@ -1,2 +1,247 @@
 from src.utils.api import channel
 from src.utils.sent_func import remove_cleantext, remove_emoji, remove_emoticons
+
+from src.utils.api import api_youtube_popular
+from src.utils.api import channel
+import pandas as pd
+
+# Tokenization
+from PyKomoran import *
+
+
+youtube_popular = api_youtube_popular(name='youtube_popular', environment='youtube', max_result=20)
+
+#Channel
+channel = channel(cha_name='슈카월드')
+channel.search()
+video_info = channel.video(channel_id='UCsJ6RuBiTVWRX156FVbeaGg')
+video_info_sub = channel.title_find(find='코로나')
+video_comment = channel.comment()
+
+# Sample
+sample_comment = video_comment.Comment[1]
+
+# Tokenization
+komoran = Komoran("EXP")
+sample_comment = komoran.get_morphes_by_tags(sample_comment, tag_list=['NNP', 'NNG', 'SF'])
+
+###########################################
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import re
+
+import torch, gc
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import gluonnlp as nlp
+import numpy as np
+
+from KoBERT.kobert.utils import get_tokenizer
+from KoBERT.kobert.pytorch_kobert import get_pytorch_kobert_model
+
+from transformers import AdamW
+from transformers.optimization import get_cosine_schedule_with_warmup
+
+gc.collect()
+torch.cuda.empty_cache()
+
+tuning_data = pd.read_csv('data/한국어_단발성_대화_데이터셋.csv')
+torch.cuda.is_available()
+torch.cuda.device_count()
+
+# Converting emotion to numeric figure
+tuning_data.loc[(tuning_data['Emotion'] == "공포"), 'Emotion'] = 0  #공포 => 0
+tuning_data.loc[(tuning_data['Emotion'] == "놀람"), 'Emotion'] = 1  #놀람 => 1
+tuning_data.loc[(tuning_data['Emotion'] == "분노"), 'Emotion'] = 2  #분노 => 2
+tuning_data.loc[(tuning_data['Emotion'] == "슬픔"), 'Emotion'] = 3  #슬픔 => 3
+tuning_data.loc[(tuning_data['Emotion'] == "중립"), 'Emotion'] = 4  #중립 => 4
+tuning_data.loc[(tuning_data['Emotion'] == "행복"), 'Emotion'] = 5  #행복 => 5
+tuning_data.loc[(tuning_data['Emotion'] == "혐오"), 'Emotion'] = 6  #혐오 => 6
+
+# Converting into list
+data_list = []
+for q, label in zip(tuning_data['Sentence'], tuning_data['Emotion'])  :
+    data = []
+    data.append(q)
+    data.append(str(label))
+
+    data_list.append(data)
+
+# SPLIT
+dataset_train, dataset_test = train_test_split(data_list, test_size=0.25, random_state=0) # Actual Train & Test
+
+#
+device = torch.device('cuda')
+bertmodel, vocab = get_pytorch_kobert_model()
+
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
+                 pad, pair):
+        transform = nlp.data.BERTSentenceTransform(
+            bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return (self.sentences[i] + (self.labels[i], ))
+
+    def __len__(self):
+        return (len(self.labels))
+
+## Setting parameters
+max_len = 64
+batch_size = 32
+warmup_ratio = 0.1
+num_epochs = 5
+max_grad_norm = 1
+log_interval = 200
+learning_rate =  5e-5
+
+data_train = BERTDataset(dataset_train, 0, 1, tok, max_len, True, False)
+data_test = BERTDataset(dataset_test, 0, 1, tok, max_len, True, False)
+
+train_dataloader = torch.utils.data.DataLoader(data_train, batch_size=batch_size)
+test_dataloader = torch.utils.data.DataLoader(data_test, batch_size=batch_size)
+
+
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                 bert,
+                 hidden_size=768,
+                 num_classes=7,  ##클래스 수 조정##
+                 dr_rate=None,
+                 params=None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
+
+        self.classifier = nn.Linear(hidden_size, num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
+
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
+
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+
+        _, pooler = self.bert(input_ids=token_ids, token_type_ids=segment_ids.long(),
+                              attention_mask=attention_mask.float().to(token_ids.device), return_dict=False)
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
+
+model = BERTClassifier(bertmodel, dr_rate=0.5).to(device)
+
+# Prepare optimizer and schedule (linear warmup and decay)
+no_decay = ['bias', 'LayerNorm.weight']
+optimizer_grouped_parameters = [
+    {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+    {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+]
+
+# Adam Optimizer
+optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate)
+loss_fn = nn.CrossEntropyLoss()
+
+t_total = len(train_dataloader) * num_epochs
+warmup_step = int(t_total * warmup_ratio)
+
+scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=warmup_step, num_training_steps=t_total)
+
+def calc_accuracy(X,Y):
+    max_vals, max_indices = torch.max(X, 1)
+    train_acc = (max_indices == Y).sum().data.cpu().numpy()/max_indices.size()[0]
+    return train_acc
+
+for e in range(num_epochs):
+    train_acc = 0.0
+    test_acc = 0.0
+    model.train()
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(train_dataloader):
+        optimizer.zero_grad()
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+        valid_length = valid_length
+        label = label.long().to(device)
+        out = model(token_ids, valid_length, segment_ids)
+        loss = loss_fn(out, label)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+        optimizer.step()
+        scheduler.step()  # Update learning rate schedule
+        train_acc += calc_accuracy(out, label)
+        if batch_id % log_interval == 0:
+            print("epoch {} batch id {} loss {} train acc {}".format(e + 1, batch_id + 1, loss.data.cpu().numpy(),
+                                                                     train_acc / (batch_id + 1)))
+    print("epoch {} train acc {}".format(e + 1, train_acc / (batch_id + 1)))
+
+    model.eval()
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+        valid_length = valid_length
+        label = label.long().to(device)
+        out = model(token_ids, valid_length, segment_ids)
+        test_acc += calc_accuracy(out, label)
+    print("epoch {} test acc {}".format(e + 1, test_acc / (batch_id + 1)))
+
+# Video Comment
+df_comment = video_comment[['Comment']]
+df_comment['Comment'] = [re.sub('[^가-힣 ]', '', s) for s in df_comment['Comment']]
+df_comment['Comment'] = df_comment.Comment.str.strip()
+idx = df_comment[df_comment['Comment'] == ''].index
+df_comment = df_comment.drop(idx).reset_index(drop=True)
+
+# Predict
+def predict(predict_sentence):
+    data = [predict_sentence, '0']
+    dataset_another = [data]
+
+    another_test = BERTDataset(dataset_another, 0, 1, tok, max_len, True, False)
+    test_dataloader = torch.utils.data.DataLoader(another_test, batch_size=batch_size)
+
+    model.eval()
+
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+
+        valid_length = valid_length
+        label = label.long().to(device)
+
+        out = model(token_ids, valid_length, segment_ids)
+
+        test_eval = []
+        for i in out:
+            logits = i
+            logits = logits.detach().cpu().numpy()
+
+            if np.argmax(logits) == 0:
+                test_eval.append("공포")
+            elif np.argmax(logits) == 1:
+                test_eval.append("놀람")
+            elif np.argmax(logits) == 2:
+                test_eval.append("분노")
+            elif np.argmax(logits) == 3:
+                test_eval.append("슬픔")
+            elif np.argmax(logits) == 4:
+                test_eval.append("중립")
+            elif np.argmax(logits) == 5:
+                test_eval.append("행복")
+            elif np.argmax(logits) == 6:
+                test_eval.append("혐오")
+
+        return test_eval[0]
+
+df_comment['predict'] = df_comment['Comment'].apply(predict)
